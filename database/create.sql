403d30d11feba91e535877b3d1ad9eee8f11f874
Rafaela Queiroz <rqueiroz2108@gmail.com>
2025-11-03 21:21:42 -0300

    feat: add order table to trading schema

--- database/create.sql
+++ database/create.sql
@@ -11,8 +11,23 @@ create table trading.account (
 );
 
 create table trading.account_asset (
-	account_id uuid,
+  account_id uuid,
 	asset_id text,
 	quantity numeric,
+	blocked_quantity numeric,
 	primary key (account_id, asset_id)
+);
+
+create table trading.order (
+	order_id uuid,
+	market_id text,
+	account_id uuid,
+	side text,
+	quantity numeric,
+	price numeric,
+	fill_quantity numeric,
+	fill_price numeric,
+	status text,
+	timestamp timestamptz,
+	primary key (order_id)
 );
